d2fa9b552851cb068c986c7b6c27cc578a791ea1
Momchil Tomov <tomov90@gmail.com>
2014-04-07 12:12:09 -0400

    PM nonfocal, high emphasis => M ~= 80-90% => YAY!!!!

--- Simulator.m
+++ Simulator.m
@@ -113,8 +113,8 @@ classdef Simulator < Model
                     activation(self.unit_id('Attend Category')) = self.MAXIMUM_ACTIVATION; % TODO ongoing task is hardcoded
                     activation(self.unit_id('Attend Syllables')) = self.MAXIMUM_ACTIVATION / 4; % TODO ongoing task is hardcoded
                     activation(self.unit_id('Word Categorization')) = self.MAXIMUM_ACTIVATION; % TODO ongoing task is hardcoded
-                    % Einstein 2005: high emph (= 1) / low emph (= 0)
-                    activation(self.unit_id('Monitor')) = 0;
+                    % Einstein 2005: high emph (= 0.5) / low emph (= 0)
+                    activation(self.unit_id('Monitor')) = 0.5;
 
                     % calculate net inputs for all units
                     self.net_input = activation * self.weights + self.bias;
