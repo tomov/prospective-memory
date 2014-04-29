94450275997e87970caa4ed4effd45e03a504c16
Momchil Tomov <tomov90@gmail.com>
2014-04-29 13:27:50 -0400

    EM2005_with_stats.m -- run that to get the nice 3x2 plot for Table 1, this time according to the right methods

--- EM2005_with_stats.m
+++ EM2005_with_stats.m
@@ -1,7 +1,7 @@
-function stat = EM2005_analyze_stats( subjects, subjects_extra)
+%function stat = EM2005_analyze_stats( subjects, subjects_extra)
 % get stats from subjects and analyze so you can fit with fitparam()
 %{
- variable order = 
+ samples variable order = 
     1 - OG_ONLY,
     2 - FOCAL, 
     3 - EMPHASIS
@@ -14,18 +14,21 @@ function stat = EM2005_analyze_stats( subjects, subjects_extra)
  (see EM2005)
 %}
 
+DO_PLOT = false;
+
+
 PM_hit = subjects(:, 7);
 
 % --- PM hit rate in focal vs. nonfocal ----
 
 PM_hit_focal = PM_hit(subjects(:, 2) == 1);
 PM_hit_nonfocal = PM_hit(subjects(:, 2) == 0);
-p = anova1([PM_hit_focal PM_hit_nonfocal]);
+%p = anova1([PM_hit_focal PM_hit_nonfocal]);
 
-PM_hit_focal_M = mean(PM_hit_focal(~isnan(PM_hit_focal)))
-PM_hit_focal_SD = std(PM_hit_focal(~isnan(PM_hit_focal)))  % / sqrt(sum(~isnan(PM_hit_focal)))
-PM_hit_nonfocal_M = mean(PM_hit_nonfocal(~isnan(PM_hit_nonfocal)))
-PM_hit_nonfocal_SD = std(PM_hit_nonfocal(~isnan(PM_hit_nonfocal)))  % / sqrt(sum(~isnan(PM_hit_nonfocal)))
+PM_hit_focal_M = mean(PM_hit_focal(~isnan(PM_hit_focal)));
+PM_hit_focal_SD = std(PM_hit_focal(~isnan(PM_hit_focal)));  % / sqrt(sum(~isnan(PM_hit_focal)))
+PM_hit_nonfocal_M = mean(PM_hit_nonfocal(~isnan(PM_hit_nonfocal)));
+PM_hit_nonfocal_SD = std(PM_hit_nonfocal(~isnan(PM_hit_nonfocal)));  % / sqrt(sum(~isnan(PM_hit_nonfocal)))
 
 
 
@@ -33,23 +36,134 @@ PM_hit_nonfocal_SD = std(PM_hit_nonfocal(~isnan(PM_hit_nonfocal)))  % / sqrt(sum
 
 PM_hit_low = PM_hit(subjects(:, 3) == 0);
 PM_hit_high = PM_hit(subjects(:, 3) == 1);
-p = anova1([PM_hit_high PM_hit_low]);
+%p = anova1([PM_hit_high PM_hit_low]);
 
-PM_hit_low_M = mean(PM_hit_low(~isnan(PM_hit_low)))
-PM_hit_low_SD = std(PM_hit_low(~isnan(PM_hit_low)))  %/ sqrt(sum(~isnan(PM_hit_low)));
-PM_hit_high_M = mean(PM_hit_high(~isnan(PM_hit_high)))
-PM_hit_high_SD = std(PM_hit_high(~isnan(PM_hit_high)))  %/ sqrt(sum(~isnan(PM_hit_high)));
+PM_hit_low_M = mean(PM_hit_low(~isnan(PM_hit_low)));
+PM_hit_low_SD = std(PM_hit_low(~isnan(PM_hit_low)));  %/ sqrt(sum(~isnan(PM_hit_low)));
+PM_hit_high_M = mean(PM_hit_high(~isnan(PM_hit_high)));
+PM_hit_high_SD = std(PM_hit_high(~isnan(PM_hit_high)));  %/ sqrt(sum(~isnan(PM_hit_high)));
 
 % --- PM hit rate in high emph vs. low emph. for different focalities ----
 % anova2? interaction between the two variables
 
 % plot em' -- for each focality, show high emph & low epmoh next to each
 % other
-p = anovan(PM_hit, {subjects(:, 2) subjects(:, 3)});
+%p = anovan(PM_hit, {subjects(:, 2) subjects(:, 3)}, 'model','interaction');
 
 
-% --- 
 
 
+
+
+% --------------------------------------------- the big plot -----------------------------------------
+
+
+
+% -------------- define the empirical stats (Table 1 from E&M 2005)
+
+%{
+ stats variable order = 
+    1 - OG_ONLY,
+    2 - FOCAL, 
+    3 - EMPHASIS
+ statistics order = 
+    4 - OG_RT_M,
+    5 - OG_RT_SD,
+    6 - OG_Hit_M,
+    7 - OG_Hit_SD,
+    8 - PM_RT_M,
+    9 - PM_RT_SD
+    10 - PM_Hit_M,
+    11 - PM_Hit_SD
+%}
+SD_cols = [5 7 9 11];
+
+subjects_per_condition = 24;
+empirical_stats = [
+    1 1 0, 1073.25, 112.04, 97, 2, NaN, NaN, NaN, NaN;  % no-PM, focal,    low emph
+    0 1 0, 1120.87, 116.48, 97, 2, NaN, NaN, 88, 16;    % PM,    focal,    low emph
+    1 1 1, 1149.25, 137.58, 97, 2, NaN, NaN, NaN, NaN;  % no-PM, focal,    high emph
+    0 1 1, 1239.17, 175.42, 97, 2, NaN, NaN, 92, 16;    % PM,    focal,    high emph
+    1 0 0, 1140.92, 172.87, 97, 2, NaN, NaN, NaN, NaN;  % no-PM, nonfocal, low emph
+    0 0 0, 1425.39, 378.52, 97, 2, NaN, NaN, 53, 34;    % PM,    nonfocal, low emph
+    1 0 1, 1183.17, 164.43, 97, 2, NaN, NaN, NaN, NaN;  % no-PM, nonfocal, high emph
+    0 0 1, 1593.43, 300.86, 97, 2, NaN, NaN, 81, 27;    % PM,    nonfocal, high emph 
+];
+
+% convert SD's to SEM's in empirical data
+empirical_stats(:, SD_cols) = empirical_stats(:, SD_cols) / sqrt(subjects_per_condition);
+
+% ------------- calculate simulation stats (Table 1 from E&M 2005)
+
+simulation_stats = [];
+for FOCAL = 1:-1:0
+    for EMPHASIS = 0:1
+        for OG_ONLY = 1:-1:0            
+            stat = [OG_ONLY, FOCAL, EMPHASIS];
+            for col = 4:7
+                samples = subjects(subjects(:, 1) == OG_ONLY & subjects(:, 2) == FOCAL & subjects(:, 3) == EMPHASIS, col);
+                M = mean(samples);
+                SD = std(samples);
+                stat = [stat, M, SD];
+            end
+            simulation_stats = [simulation_stats; stat];
+        end
+    end
 end
 
+% convert SD's to SEM's in simulation data
+simulation_stats(:, SD_cols) = simulation_stats(:, SD_cols) / sqrt(subjects_per_condition);
+
+% -------------- run linear regression to find slope and intercept for RT's
+
+empirical_RTs = empirical_stats(:, 4);
+simulation_cycles = simulation_stats(:, 4);
+
+p = polyfit(simulation_cycles, empirical_RTs, 1)
+RT_slope = p(1);
+RT_intercept = p(2);
+yfit = polyval(p, simulation_cycles);
+
+yresid = empirical_RTs - yfit;
+SSresid = sum(yresid.^2);
+SStotal = (length(empirical_RTs)-1) * var(empirical_RTs);
+rsq = 1 - SSresid/SStotal;
+
+if DO_PLOT
+    figure;
+    scatter(simulation_cycles, empirical_RTs);
+    %xlabel('Simulation RTs (cycles)');
+    %ylabel('Empirical RTs (msec)');
+    lsline
+    title(sprintf('R^2 = %.4f', rsq));
+end
+
+
+% ------------------ plot the all the good stuff
+
+
+figure;
+
+subplot(3, 2, 1);
+title('Empirical Data (Einstein & McDaniel 2005)');
+ylabel('OG RT (msec)');
+plot_all_conditions(empirical_stats(:, [1:3 4 5]), 1000, 1700, 1, 0, true);
+
+subplot(3, 2, 2);
+title('Simulation Data');
+ylabel(sprintf('OG RT (msec = cycles * %.1f + %.1f)', RT_slope, RT_intercept));
+plot_all_conditions(simulation_stats(:, [1:3 4 5]), 1000, 1700, RT_slope, RT_intercept, false);
+
+subplot(3, 2, 3);
+ylabel('OG Accuracy (%)');
+plot_all_conditions(empirical_stats(:, [1:3 6 7]), 40, 100, 1, 0, false);
+
+subplot(3, 2, 4);
+plot_all_conditions(simulation_stats(:, [1:3 6 7]), 40, 100, 1, 0, false);
+
+subplot(3, 2, 5);
+ylabel('PM Hit Rate (%)');
+plot_all_conditions(empirical_stats(:, [1:3 10 11]), 40, 100, 1, 0, false);
+
+subplot(3, 2, 6);
+plot_all_conditions(simulation_stats(:, [1:3 10 11]), 40, 100, 1, 0, false);
---
+++ plot_all_conditions.m
@@ -0,0 +1,46 @@
+function plot_all_conditions( stats, ymin, ymax, slope, intercept, show_legend)
+%UNTITLED9 Summary of this function goes here
+%   Detailed explanation goes here
+
+focal_titles = {'Nonfocal', 'Focal'};
+markers = {'b-o', 'r-*'};
+emphasis_titles = {'Low', 'High'};
+og_only_titles = {'PM', 'No-PM'};
+handles = [];
+
+legend_titles = {};
+for FOCAL = 1:-1:0
+    xticklabels = {};
+    values = [];
+    errors = [];
+    xes = [];
+    x = 1;
+    for OG_ONLY = 1:-1:0
+        for EMPHASIS = 0:1
+            stat = stats(stats(:, 1) == OG_ONLY & stats(:, 2) == FOCAL & stats(:, 3) == EMPHASIS, :);
+            M = stat(4) * slope + intercept;
+            SD = stat(5) * slope;
+            values = [values, M];
+            errors = [errors, SD];
+            xes = [xes, x];
+            x = x + 1;
+            xlabel = sprintf('%s, %s', emphasis_titles{EMPHASIS+1}, og_only_titles{OG_ONLY+1});
+            xticklabels = [xticklabels,  {xlabel}];
+        end
+    end
+    hold on;
+    errorbar(xes, values, errors);
+    handle = plot(xes, values, markers{FOCAL+1}, 'LineWidth', 2, 'MarkerSize', 6);
+    handles = [handles, handle];
+    legend_titles = [legend_titles; focal_titles(FOCAL+1)];
+end
+hold off;
+axis([0 x ymin ymax]);
+set(gca, 'XTickLabel', [{''}, xticklabels, {''}]);
+if show_legend
+    legend(handles, legend_titles);
+end
+
+
+
+
